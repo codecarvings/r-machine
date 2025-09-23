b72505566ba47a757cb2db0cf7739fa8160982eb
Sergio Turolla <git@codecarvings.com>
2025-09-23 13:42:18 +0200

    chore: update pnpm workspace configuration and add TypeScript support

--- packages/r-machine/jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "jest";
-
-const config: Config = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/src/**/*.test.ts"],
-};
-
-export default config;
