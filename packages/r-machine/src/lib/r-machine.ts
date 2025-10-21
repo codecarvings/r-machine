190cb48330606b5057e6818ce35d5ae7f8123e6e
Sergio Turolla <turolla@users.noreply.github.com>
2025-10-21 10:20:13 +0200

    Add GitHub Actions workflow for testing and versioning (#3)
    * feat: add GitHub Actions workflow for testing
    
    * feat: add changeset initialization
    
    * feat: remove version check from CI workflow

--- packages/r-machine/src/lib/r-machine.ts
+++ packages/r-machine/src/lib/r-machine.ts
@@ -4,7 +4,6 @@ import { LocaleMapperManager } from "./locale-mapper-manager.js";
 import type { AnyAtlas, AtlasNamespace } from "./r.js";
 import type { AtlasNamespaceList, RKit } from "./r-kit.js";
 import { cloneRMachineConfig, type RMachineConfig, validateRMachineConfig } from "./r-machine-config.js";
-import { version } from "./version.js";
 
 export class RMachine<A extends AnyAtlas> {
   constructor(config: RMachineConfig) {
@@ -45,6 +44,4 @@ export class RMachine<A extends AnyAtlas> {
     const domain = this.domainManager.getDomain(mappedLocale);
     return domain.pickRKit(namespaces) as RKit<A, NL> | Promise<RKit<A, NL>>;
   };
-
-  static readonly version = version;
 }
--- packages/r-machine/src/lib/version.ts
+++
@@ -1,5 +0,0 @@
-export const version = {
-  major: 1,
-  minor: 0,
-  patch: "0-alpha.1",
-} as const;
