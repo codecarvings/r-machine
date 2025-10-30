315569ec8cb4e9459d30e06157dc7882c30918de
Sergio Turolla <turolla@users.noreply.github.com>
2025-10-30 16:54:46 +0100

    Fix import errors in tests when package directories are  clean from built artifacts (#14)

--- packages/r-machine-next/vitest.config.ts
+++ packages/r-machine-next/vitest.config.ts
@@ -1,5 +1,10 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineProject, mergeConfig, type ViteUserConfig } from "vitest/config";
 import baseConfig from "../../configs/vitest.base.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default mergeConfig(
   baseConfig,
@@ -9,6 +14,12 @@ export default mergeConfig(
         tsconfig: "./tsconfig.test.json",
         // ignoreSourceErrors: true,
       },
+    },
+    resolve: {
+      alias: [
+        { find: /^#r-machine\/next$/, replacement: path.resolve(__dirname, "./src/lib/index.ts") },
+        { find: /^#r-machine\/next\/(.*)$/, replacement: path.resolve(__dirname, "./src/$1/index.ts") },
+      ],
     },
   })
 ) as ViteUserConfig;
