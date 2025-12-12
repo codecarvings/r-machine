5bfc56341ae6d2b965f34ff7224d1d9ae24599bc
Sergio Turolla <git@codecarvings.com>
2025-12-12 20:35:28 +0100

    chore: minor gfx changes in examples

--- examples/next-with-app-path-strategy-no-proxy/src/components/client/locale-switcher.tsx
+++ examples/next-with-app-path-strategy-no-proxy/src/components/client/locale-switcher.tsx
@@ -35,7 +35,7 @@ export function LocaleSwitcher() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="default" className="gap-2 font-semibold" id="locale-switcher-button">
+        <Button variant="ghost" size="default" className="gap-2 font-semibold" id="locale-switcher-button">
           <Languages className="size-5" />
           <span className="hidden sm:inline text-base">{currentLocaleItem.name}</span>
         </Button>
