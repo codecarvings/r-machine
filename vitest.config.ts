1af613396a86d1e19ad0265fee44b762ebb1a865
Sergio Turolla <git@codecarvings.com>
2026-02-01 21:21:01 +0100

    test: add test coverage reports

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,8 @@ import { defineConfig, type ViteUserConfig } from "vitest/config";
 export default defineConfig({
   test: {
     projects: ["packages/*"],
+    coverage: {
+      reporter: ["text", "html"],
+    },
   },
 }) as ViteUserConfig;
