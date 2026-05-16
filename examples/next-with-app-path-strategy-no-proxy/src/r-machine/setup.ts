108260d942a4a471fc03a5eae64ee53f66eb5837
Sergio Turolla <git@codecarvings.com>
2026-05-16 13:54:44 +0200

    feat: add createNextDevImport to next examples

--- examples/next-with-app-path-strategy-no-proxy/src/r-machine/setup.ts
+++ examples/next-with-app-path-strategy-no-proxy/src/r-machine/setup.ts
@@ -1,13 +1,16 @@
 import { NextAppPathStrategy } from "@r-machine/next/app/path";
+import { createNextDevImport } from "@r-machine/next/dev";
 import { RMachine, type RMachineLocale } from "r-machine";
 import { PathAtlas } from "./path-atlas";
 import { ResourceAtlas } from "./resource-atlas";
+
+const devImport = await createNextDevImport(import.meta.url);
 
 const rMachine = RMachine.create({
   locales: ["en", "it-IT"],
   defaultLocale: "en",
   ResourceAtlas,
-  load: (path) => import(`./${path}`),
+  load: (path) => (devImport ? devImport(`./${path}`) : import(`./${path}`)),
   shellKit: {
     fmt: "shell/lib/fmt",
   },
